1632c8d143df8ee62fa9f21c5f1644807336fd67
sp-yduck <sudo20.t.ab@gmail.com>
2024-12-31 12:43:38 +0000

    remove nav buttons

--- src/components/calendar/CalendarHeatmap.tsx
+++ src/components/calendar/CalendarHeatmap.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 export default function CalendarHeatmap() {
-  return <Calendar numberOfMonths={12} />;
+  return <Calendar numberOfMonths={12} classNames={{ nav: "hidden" }} />;
 }
