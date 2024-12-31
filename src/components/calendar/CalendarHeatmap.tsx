9c6bf6cfe47ccdf2d44338e39769d232c4c41dcf
sp-yduck <sudo20.t.ab@gmail.com>
2024-12-31 12:42:48 +0000

    show 12 months

--- src/components/calendar/CalendarHeatmap.tsx
+++ src/components/calendar/CalendarHeatmap.tsx
@@ -2,14 +2,5 @@ import React from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 export default function CalendarHeatmap() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
-
-  return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      numberOfMonths={3}
-    />
-  );
+  return <Calendar numberOfMonths={12} />;
 }
