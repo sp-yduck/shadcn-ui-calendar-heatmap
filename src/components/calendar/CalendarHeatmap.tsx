8a7c21a6cc17ad0ee384ff99ff82af33188d77c8
sp-yduck <sudo20.t.ab@gmail.com>
2024-12-31 12:45:05 +0000

    reverse row and column

--- src/components/calendar/CalendarHeatmap.tsx
+++ src/components/calendar/CalendarHeatmap.tsx
@@ -2,5 +2,14 @@ import React from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 export default function CalendarHeatmap() {
-  return <Calendar numberOfMonths={12} classNames={{ nav: "hidden" }} />;
+  return (
+    <Calendar
+      numberOfMonths={12}
+      classNames={{
+        nav: "hidden",
+        tbody: "flex",
+        row: "[user-select:none;] flex flex-col",
+      }}
+    />
+  );
 }
