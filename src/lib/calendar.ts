3e4fe857bc26afa3d9754bc06480f92bc5128f8e
sp-yduck <sudo20.t.ab@gmail.com>
2025-01-01 01:49:34 +0000

    set hours to zero so to make it properly comparable by toISOString()

--- src/lib/calendar.ts
+++ src/lib/calendar.ts
@@ -2,11 +2,18 @@ import { type CalendarHeatmapData } from "../components/calendar/CalendarHeatmap
 
 // generate random datas for calendar heatmap with count from 0 to 4
 // for the last 365 days
-export function generateRandomDatas() {
+export function generateRandomDatas(): CalendarHeatmapData[] {
   const datas: CalendarHeatmapData[] = [];
   for (let i = 0; i < 365; i++) {
     datas.push({
-      date: new Date(new Date().setDate(new Date().getDate() - i)),
+      date: new Date(
+        new Date(new Date().setDate(new Date().getDate() - i)).setHours(
+          0,
+          0,
+          0,
+          0
+        )
+      ),
       count: Math.floor(Math.random() * 5),
     });
   }
